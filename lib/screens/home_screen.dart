1b5f59da823ef972ddbd0eaa56f8ec27d46b8180
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-05 13:53:40 +0530

    basic

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/utils/colors.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: appPrimaryColor,
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color appPrimaryColor = Color(0xFF191414);
+const Color appTextColor = Color(0xFFEBECED);
+const Color appAccentColor = Color(0xFF1DB954);
